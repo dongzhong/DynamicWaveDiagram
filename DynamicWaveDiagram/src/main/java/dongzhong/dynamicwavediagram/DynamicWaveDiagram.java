c9b013e607c6eae398a2a93221b8c9e36e1dbefc
dongzhong1990 <dongzhong1990@foxmail.com>
2017-12-21 01:38:03 +0800

    save

--- DynamicWaveDiagram/src/main/java/dongzhong/dynamicwavediagram/DynamicWaveDiagram.java
+++ DynamicWaveDiagram/src/main/java/dongzhong/dynamicwavediagram/DynamicWaveDiagram.java
@@ -6,23 +6,40 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by dongzhong on 2017/12/20.
  */
 
 public class DynamicWaveDiagram extends View {
+    private int updateTime = 500;
+    private int pointNum = 100;
+    private float ceilValue = 10.0f;
+    private float floorValue = -10.0f;
+
+    private int left = 0;
+    private int right = 0;
+    private int top = 0;
+    private int bottom = 0;
+
+    private Timer drawTimer = new Timer();
 
     /********************** Constructor **********************/
     public DynamicWaveDiagram(Context context) {
         super(context);
+        init();
     }
 
     public DynamicWaveDiagram(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public DynamicWaveDiagram(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     /**********************************************************/
@@ -46,8 +63,6 @@ public class DynamicWaveDiagram extends View {
         final int paddingRight = getPaddingRight();
         final int paddingTop = getPaddingTop();
         final int paddingBottom = getPaddingBottom();
-
-        int left, right, top, bottom;
         if (getWidth() >= paddingLeft + paddingRight) {
             left = paddingLeft;
             right = getWidth() - paddingRight;
@@ -64,5 +79,35 @@ public class DynamicWaveDiagram extends View {
             top = getHeight() / 2;
             bottom = getHeight() / 2;
         }
+    }
+
+    private void init() {
+        if (drawTimer != null) {
+            drawTimer.cancel();
+        }
+        drawTimer = new Timer();
+        drawTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                updateDate();
+            }
+        }, 0, updateTime);
+    }
+
+    /**
+     * 更新数据
+     */
+    private void updateDate() {
+
+        invalidate();
+    }
+
+    /**
+     * 绘制动态波浪图
+     * @param canvas
+     */
+    private void drawDiagram(Canvas canvas) {
+
+
     }
 }
